5d9412ef13432c730a0c6a3981b72800d85f2b25
ElenaG <freshelf@yandex.ru>
2023-06-21 22:57:04 +0500

    HW_5 is completed

---
+++ HW_5/task_3.py
@@ -0,0 +1,23 @@
+"""
+Задача 3.
+Создайте функцию генератор чисел Фибоначчи (см. Википедию).
+"""
+from time import sleep
+
+
+def fibonacci_gen():
+    x = 0
+    y = 1
+    while True:
+        yield x + y
+        sleep(2)
+        y = x + y
+        x = y - x
+
+
+def print_fib(num):
+    for num in fibonacci_gen():
+        print(f'{num}\n')
+
+
+print_fib(fibonacci_gen())
